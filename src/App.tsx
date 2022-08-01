5f44dcddfa8554bc3b48ab1b4562392a7fe1dd5a
Vladimir Golub <golubvlauri@gmail.com>
2022-08-01 12:04:13 +0200

    add scss, Bootstrap, router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+      <Button>Sample Btn</Button>
     </div>
   );
 }
